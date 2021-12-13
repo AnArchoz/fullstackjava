df7f230b821dcabd6efad4241cc9f7d54650f913
AnArchoz <antoinerebelo94@gmail.com>
2021-12-13 10:05:50 +0100

    Fully working calculator for 2 numbers, but no optional features implemented.

---
+++ Main.java
@@ -0,0 +1,94 @@
+import java.util.Scanner;
+
+public class Main {
+    private static Scanner user;
+    private final String[] operations = {"+", "-", "/", "*"};
+
+    public static void main(String[] args) {
+        Main app = new Main();
+        user = new Scanner(System.in);
+        app.start();
+    }
+
+    public void start() {
+        System.out.println("Please enter two numbers and an operation, or type 'quit' to end the program.");
+        String input = user.nextLine().toLowerCase();
+
+        // End program if input = quit
+        if (input.equals("quit")) {
+            System.out.println("Good bye!");
+            System.exit(0);
+
+            // TRYING TO VALIDATE WITH REGEX BUT NOT GOING WELL HELP
+            //            if (!input.matches("^[\\s0-9*/+-]$")) {
+            //              System.out.println("Enter only add numbers and operations");
+            //            start();
+            //    }
+
+        } else {
+            // Check if string contains numbers and operators
+            String[] splitstring = input.split("\\s+");
+            boolean found = false;
+            int number1 = 0, number2 = 0;
+            String operator;
+            // Check if string contains exactly two numbers and operators
+            if (splitstring.length != 3) {
+                System.out.println("please enter correctly!");
+                start();
+            }
+
+            try {
+                // Check if numbers are numbers and not text
+                number1 = Integer.parseInt(splitstring[0]);
+                number2 = Integer.parseInt(splitstring[2]);
+            } catch (NumberFormatException e) {
+                System.out.println("You did not enter correct numbers");
+                start();
+            }
+            for (String operation : operations) {
+                // Check if operator exists
+                if (splitstring[1].equals(operation)) {
+                    found = true;
+                    break;
+                }
+            }
+            if (!found) {
+                System.out.println("Please use only /*'- operators");
+                start();
+            }
+
+            // Assign numbers and operators properly and send to calculation
+            operator = splitstring[1];
+            double result = calculate(operator, number1, number2);
+            System.out.println("The result is: " + result);
+
+            // Lets user calculate another number
+            start();
+        }
+    }
+
+    /**
+     * CALCULATES THE NUMBERS AND RETURNS A DOUBLE
+     */
+    public double calculate(String operator, int number1, int number2) {
+
+        switch (operator) {
+            case "*":
+                return number1 * number2;
+            case "+":
+                return number1 + number2;
+            case "-":
+                return number1 - number2;
+            case "/":
+                if (number1 == 0 | number2 == 0) {
+                    System.out.println("can't divide with 0!!!");
+                    return -1;
+                } else {
+                    return (double) number1 / number2;
+                }
+
+        }
+
+        return -1;
+    }
+}
