a9f2533e46f90e2ce973ee270daa479f4cdf3416
AnArchoz <antoinerebelo94@gmail.com>
2021-12-14 16:49:33 +0100

    TODO: Implement dynamic array, implement PEMDAS-rule, implement JUnit tests.

--- Main.java
+++ Main.java
@@ -10,6 +10,10 @@ public class Main {
         app.start();
     }
 
+    /**
+     * VALIDATES THE COMMAND THE USER INPUTS, IN ORDER TO PROPERLY CALCULATE NUMBERS USING ADDITION,
+     * SUBTRACTION, DIVISION, AND MULTIPLICATION FOLLOWING THE PEMDAS RULE OF HIERARCHY.
+     */
     public void start() {
         System.out.println("Please enter two numbers and an operation with spaces in between, or type 'quit' to end the program.");
         String input = user.nextLine().toLowerCase();
@@ -19,49 +23,54 @@ public class Main {
             System.out.println("Good bye!");
             System.exit(0);
 
-        // FINALLY WORKING REGEX: Checks that string only contains numbers or the chosen operators
+            // Checks that string only contains numbers and the chosen operators
         } else if (!input.matches("^[\\s0-9*/+-]+\\d$")) {
             System.out.println("Enter only numbers and operations");
             start();
         } else {
             // Check if string contains numbers and operators
             String[] splitstring = input.split("\\s+");
-            boolean found = false;
-            int number1 = 0, number2 = 0;
-            String operator;
+            boolean operatorsValid = false;
+            boolean pemdas = false;
+            // TODO: Build dynamicArray class for holding operators
+            DynArr divMultLocations = new DynArr(new int[0]);
+
             // Check if string contains exactly two numbers and operators
-            // TODO: CHANGE FOR ADDITION AND SUBTRACTION
-            if (splitstring.length != 3) {
-                System.out.println("Please enter only two numbers and an operator");
-                start();
-            }
+            // TODO: CHANGE FOR PEMDAS
+            // if (splitstring.length != 3) {
+            //  System.out.println("Please enter only two numbers and an operator");
+            // start();
+            //}
 
             try {
-                // Check if numbers are numbers and not text
-                number1 = Integer.parseInt(splitstring[0]);
-                number2 = Integer.parseInt(splitstring[2]);
+                // Check if numbers are numbers, (as even indexes or odd elements)
+                for (int i = 0; i < splitstring.length; i += 2) {
+                    Integer.parseInt(splitstring[i]);
+                }
             } catch (NumberFormatException e) {
                 System.out.println("You did not enter correct numbers");
                 start();
             }
 
-            for (String operation : operations) {
-                // Check if operator exists
-                if (splitstring[1].equals(operation)) {
-                    found = true;
-                    break;
+            for (int i = 0, j = 0; i < operations.length; i++) {
+                String operation = operations[i];
+                // Check if operator exists (as odd indexes or even elements)
+                operatorsValid = i % 2 != 0 | splitstring[i].equals(operation);
+                if (operation.equals("/") || operation.equals("*")) {
+                    j++;
+                    pemdas = true;
+                    // TODO: Implement dynamic array
+                    divMultLocations.setElement(i);
                 }
             }
 
-            if (!found) {
-                System.out.println("Please use only /*'- operators");
+            // If operators are incorrect, restart
+            if (!operatorsValid) {
+                System.out.println("Please use only /*'- operators, one per operation");
                 start();
             }
 
-            // Assign numbers and operators properly and send to calculation
-            // TODO: Maybe probably move to calculate method
-            operator = splitstring[1];
-            double result = calculate(operator, number1, number2);
+            double result = calculate(splitstring, pemdas);
             System.out.println("The result is: " + result);
 
             // Lets user calculate another number
@@ -70,30 +79,29 @@ public class Main {
     }
 
     /**
-     *
-     * CALCULATES THE int NUMBERS AND RETURNS A DOUBLE for numbers with decimal points
+     * CALCULATES THE int NUMBERS AND RETURNS A double FOR NUMBERS WITH DECIMAL POINTS
      */
 
-
-    public double calculate(String operator, int number1, int number2) {
-
-        switch (operator) {
-            case "*":
-                return number1 * number2;
-            case "+":
-                return number1 + number2;
-            case "-":
-                return number1 - number2;
-            case "/":
-                if (number1 == 0 | number2 == 0) {
-                    System.out.println("can't divide with 0!!!");
-                    return -1;
-                } else {
-                    return (double) number1 / number2;
-                }
-
-        }
-
-        return -1;
-    }
+    // TODO: REBUILD COMPLETELY
+    public double calculate(String[] statement, boolean pemdas) {
+//        double result = 0;
+//        switch (operator) {
+//            case "*":
+//                return number1 * number2;
+//            case "+":
+//                return number1 + number2;
+//            case "-":
+//                return number1 - number2;
+//            case "/":
+//                if (number1 == 0 | number2 == 0) {
+//                    System.out.println("can't divide with 0!!!");
+//                    return -1;
+//                } else {
+//                    return (double) number1 / number2;
+//                }
+//
+//        }
+//
+//        return -1;
+//    }
 }
